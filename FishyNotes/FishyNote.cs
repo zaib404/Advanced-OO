8418cd1bc9e9d288badaed4cbbab9a37a2452b86
Zaib <inam1_16@uni.worc.ac.uk>
2020-01-20 11:31:09 +0000

    a

--- FishyNotes/FishyNote.cs
+++ FishyNotes/FishyNote.cs
@@ -50,5 +50,48 @@ namespace FishyNotes
         {
             automagically = textBox1.Text;
         }
+
+
+        #region Code Snippet: makes this borderless window movable
+        // Modified from https://stackoverflow.com/a/24561946 and attributed to user jay_t55
+
+        // DECLARE a boolean that is set to true when a mouse down event is detected, call it _mouseDown:
+        private bool _mouseDown = false;
+
+        // DECLARE a Point object to store the last location of the window origin (ie memory), call it _lastLocation:
+        private Point _lastLocation;
+
+        // Mouse down event handler
+        private void FishyNote_MouseDown(object sender, MouseEventArgs e)
+        {
+            // SET _mouseDown true:
+            _mouseDown = true;
+
+            // STORE current location to _lastLocation:
+            _lastLocation = e.Location;
+        }
+
+        // Mouse move event handler
+        private void FishyNote_MouseMove(object sender, MouseEventArgs e)
+        {
+            // Only act if mouse button is down...
+            if (_mouseDown)
+            {
+                // SET new location according to mouse movement from _lastLocation:
+                this.Location = new Point(
+                    (this.Location.X - _lastLocation.X) + e.X, (this.Location.Y - _lastLocation.Y) + e.Y);
+
+                // UPDATE this window:
+                this.Update();
+            }
+        }
+
+        // Mouse up event handler
+        private void FishyNote_MouseUp(object sender, MouseEventArgs e)
+        {
+            // RESET _mouseDown to false:
+            _mouseDown = false;
+        }
+        #endregion
     }
 }
